1815f073d9f368d4a4e6c059f1d1c2447903ad45
Likith H M <158479878+LikiTH1233@users.noreply.github.com>
2026-02-16 22:08:30 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,14 @@ export default function Home() {
       <div className="flex flex-col items-center mt-40">
         <h1 className="text-3xl mb-6 font-bold">Smart Bookmark App</h1>
         <button
-          onClick={() => supabase.auth.signInWithOAuth({ provider: 'google' })}
+         onClick={() =>
+  supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: window.location.origin
+    }
+  })
+}
           className="bg-blue-600 text-white px-6 py-3 rounded"
         >
           Sign in with Google
